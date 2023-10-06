d64affd2fb67eebb1cb71f290588c214935785be
Sangam Prasad <80503389+SangamPrasad2000@users.noreply.github.com>
2023-10-07 04:06:46 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "/Users/sangam/Downloads/Portfolio-master/src/Assets/Sangam Prasad Resume copy.pdf";
+import pdf from "/Users/sangam/Downloads/sangams-portfolio/src/Assets/Sangam Prasad Resume copy.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
